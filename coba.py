4d2f5c464c35a89632b5b087eb7abc90ef275b53
didanendya <didanendya@ti.ukdw.ac.id>
2022-11-09 20:50:49 +0700

    tambah tipe data

---
+++ coba.py
@@ -0,0 +1,4 @@
+roll_number = 1234
+marks = 52.5
+print ("Datatype for roll_number is",type(roll_number))
+print ("Datatype for marks is",type(marks)) #Notice that we did not declare either of the variables with a particular datatype
