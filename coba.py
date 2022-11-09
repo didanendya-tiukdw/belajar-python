0f037a433650cc97eacfd4131a4082a0463a90f2
didanendya <didanendya@ti.ukdw.ac.id>
2022-11-09 21:07:03 +0700

    tambah data list

--- coba.py
+++ coba.py
@@ -1,4 +1,8 @@
 roll_number = 1234
 marks = 52.5
+a = "tina"
+b = [1,2,3]
 print ("Datatype for roll_number is",type(roll_number))
-print ("Datatype for marks is",type(marks)) #Notice that we did not declare either of the variables with a particular datatype
+print ("Datatype for marks is",type(marks)) 
+print ("Datatype for a is",type(a)) 
+print ("Datatype for a is",type(b)) 
